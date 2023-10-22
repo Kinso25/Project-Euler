b2671b8534bad66c82549de2075172b767597823
Kinso25 <wilkothewild@gmail.com>
2023-10-23 00:43:14 +1100

    Update projectEuler#15.py
    Finished project 15

--- projectEuler#15.py
+++ projectEuler#15.py
@@ -14,3 +14,37 @@ see if there is patterns
 0 0 0 
 
 '''
+
+'''
+1100
+1010
+1001
+0110
+0101
+0011
+'''
+import numpy as np
+size = 21
+matrix = np.zeros((size, size))
+
+# Setup for matrix 
+for i in range(size):
+    matrix[i, 0] = 1
+for i in range(size):
+    matrix[0, i] = 1
+
+print(matrix)
+print(" ")
+
+for row in range(1, size):
+    for col in range(1, size):
+        matrix[row, col] = matrix[row - 1, col] + matrix[row, col - 1]
+
+for i in range(20):
+    print(matrix[i])
+
+
+
+print(matrix[size-1, size-1])
+
+
