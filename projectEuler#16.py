da61f1d57e28a35296f233872432ac054c646be4
Kinso25 <wilkothewild@gmail.com>
2023-10-24 13:50:39 +1100

    Completed problem 16

--- projectEuler#16.py
+++ projectEuler#16.py
@@ -1,39 +1,13 @@
-# 2^1000 is too big a number to add all the digits to so there has to be some sort of trick
+#I completely overthought this and thought it was asking for the sum of all digit of the powers of 2 up to 2^1000
+#but this was pretty easy
 import math
 
 def getSum(n):
-    strr = str(n)
-    list_of_number = list(map(int, strr.strip()))
-    return sum(list_of_number)
+    strn = str(n)
+    listn = list(map(int, strn.strip()))
+    return sum(listn)
 
-def square(n):
-    return pow(2,n)
-
-size = 5
-numbers = [0] * size
-for i in range(size):
-    numbers[i] = i+1
-
-test = map(square, numbers)
-test = map(getSum,test)
-
-result = list(test)
-total = sum(result)
-'''
-totalList = [0] *(size+1)
-powerTotalMain = 0
-
-for i in range(size+1):
-    x = pow(2,i)
-    powerTotal = getSum(x)
-    powerTotalMain = powerTotalMain + powerTotal
-    print(i)
-    totalList[i] = powerTotal
-    print(pow(2,i),powerTotal,powerTotalMain)
-print(totalList)
-'''
-
-#print(numbers)
-print(numbers)
-print(result) 
-print(total)
+bigNum = pow(2,1000)
+print(bigNum) 
+result = getSum(bigNum)
+print(result)
